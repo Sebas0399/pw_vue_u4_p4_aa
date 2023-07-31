19dce95d6c74390ff5a48b427f5d0177e575587e
Sebas0399 <antony.aguay@gmail.com>
2023-07-31 08:18:18 -0500

    first commit

---
+++ src/modules/estudiante/pages/Estudiante.vue
@@ -0,0 +1,19 @@
+<template>
+  <DatosEstudiante/>
+</template>
+
+<script>
+
+import DatosEstudiante from '../components/DatosEstudiante.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DatosEstudiante
+  }
+}
+</script>
+
+<style>
+
+</style>
