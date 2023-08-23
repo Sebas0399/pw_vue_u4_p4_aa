4eadc4c2a8207523c7b763d02887d611d07580ab
Sebas0399 <antony.aguay@gmail.com>
2023-08-22 19:40:22 -0500

    consumo token quemado

--- src/modules/estudiante/helpers/EstudianteCliente.js
+++ src/modules/estudiante/helpers/EstudianteCliente.js
@@ -1,5 +1,6 @@
 import axios from "axios"
 const url = "http://localhost:8080/API/v1.0/Matricula/estudiantes"
+const token="eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJBbnRvbnkiLCJpYXQiOjE2OTI3NTA2NjQsImV4cCI6MTY5Mjc1MTY2NH0.ApCgIlJt1x6sZ9RYFxb78gzHZKmKdKTV3vdKSaVeR8D8S_cltT_LW597L87IPI6wy6tXU0BITtbY0Vm2XJIf4A"
 
 export const obtenerEstudianteFachada = async (cedula) => {
 
@@ -26,34 +27,33 @@ export const obtenerEstudianteAPI = async (cedula) => {
     console.log(data);
 }
 
-export const ingresarEstudianteAPI = (bodyEstudiante) => {
-
-}
-
-export const actualizarEstudianteAPI = (bodyEstudiante, id) => {
-
-}
-
-export const eliminarEstudianteAPI = (id) => {
-
-}
 const obtenerEstudianteAPIAxios = async (cedula) => {
-    console.log("Axios")
-    const data = axios.get(url + "/" + cedula).then(r => r.data)
-    console.log(data)
+    const headers={
+        "Authorization":"Bearer "+token ,
+        "Mensaje":"Valor1"
+    }
+    const data = axios.get(url + "/" + cedula,{headers:headers}).then(r => r.data)
     return data
 
 }
 const ingresarEstudianteAPIAxios = (bodyEstudiante) => {
-    axios.post(url, bodyEstudiante).then(r => r.data)
+    const headers={
+        "Authorization":"Bearer "+token ,
+        "Mensaje":"Valor1"
+    }
+    axios.post(url, bodyEstudiante,{headers:headers}).then(r => r.data)
 }
 const actualizarEstudiante = (bodyEstudiante, id) => {
-    console.log(id)
-
-    console.log(bodyEstudiante)
-    console.log(url, +"/" + id)
-    axios.put(url + "/" + id, bodyEstudiante).then(r => r.data)
+    const headers={
+        "Authorization":"Bearer "+token ,
+        "Mensaje":"Valor1"
+    }
+    axios.put(url + "/" + id, bodyEstudiante,{headers:headers}).then(r => r.data)
 }
 const eliminarEstudiante = async (id) => {
-    axios.delete(url + "/" + id).then(r => r.data)
+    const headers={
+        "Authorization":"Bearer "+token ,
+        "Mensaje":"Valor1"
+    }
+    axios.delete(url + "/" + id,{headers:headers}).then(r => r.data)
 }
